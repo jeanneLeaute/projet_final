60700cb21c05b399dc14d19bfb4064e69b2e0368
richard <richard.kumeka@hotmail.fr>
2023-04-11 14:51:29 +0200

    commentaireV8

--- ProjetFinalRest/src/main/java/projetFinalRest/restcontroller/CommentaireRestController.java
+++ ProjetFinalRest/src/main/java/projetFinalRest/restcontroller/CommentaireRestController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -25,6 +27,8 @@ import projetFinal.entities.Restaurant;
 import projetFinal.entities.views.JsonViews;
 import projetFinal.services.CommentaireService;
 
+@RestController
+@RequestMapping("/api/commentaire")
 public class CommentaireRestController {
 
 	@Autowired
