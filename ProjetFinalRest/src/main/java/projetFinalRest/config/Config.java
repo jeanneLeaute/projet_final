c9ef2285a7807eeb62a17586c2ae98be98eb1bb0
leo paillat <leopaillat@yhoo.fr>
2023-04-11 11:03:39 +0200

    config

--- ProjetFinalRest/src/main/java/projetFinalRest/config/Config.java
+++ ProjetFinalRest/src/main/java/projetFinalRest/config/Config.java
@@ -1,5 +1,16 @@
 package projetFinalRest.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import projetFinal.config.JpaConfig;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ProjetFinal.restcontroller")
+@Import(JpaConfig.class)
 public class Config {
 
 }
